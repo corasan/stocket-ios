256f2025c1580a57be7ed163dff447bcc4218c81
Henry Paulino <henrypl360@gmail.com>
2020-02-22 17:34:12 -0500

    Initial Commit

---
+++ StocketiOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StocketiOS
+//
+//  Created by Henry Paulino on 2/22/20.
+//  Copyright © 2020 Henry Paulino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
