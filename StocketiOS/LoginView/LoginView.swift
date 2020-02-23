775db7ec3b03e28d9786c658f25c84d48da38c07
Henry Paulino <henrypl360@gmail.com>
2020-02-22 21:39:07 -0500

    add colors

---
+++ StocketiOS/LoginView/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  Production
+//
+//  Created by Henry Paulino on 2/22/20.
+//  Copyright © 2020 Henry Paulino. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
