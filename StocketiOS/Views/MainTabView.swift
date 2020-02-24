a993f9cd4243b9d987ba605fdc4312ceef8fe3d0
Henry Paulino <henrypl360@gmail.com>
2020-02-24 14:48:21 -0500

    add main tab vie

---
+++ StocketiOS/Views/MainTabView.swift
@@ -0,0 +1,26 @@
+//
+//  MainView.swift
+//  Production
+//
+//  Created by Henry Paulino on 2/22/20.
+//  Copyright © 2020 Henry Paulino. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            HomeView().tabItem({
+                Text("Home")
+            })
+        }
+        .background(StocketColors.black.edgesIgnoringSafeArea(.all))
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
