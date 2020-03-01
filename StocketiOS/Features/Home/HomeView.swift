4fabd571d8c285b12071dddf958ff79de618b9fa
Henry Paulino <henrypl360@gmail.com>
2020-03-01 17:11:35 -0500

    set background color in home

--- StocketiOS/Features/Home/HomeView.swift
+++ StocketiOS/Features/Home/HomeView.swift
@@ -42,6 +42,7 @@ struct HomeView: View {
                 Spacer()
             }
             .navigationBarTitle("Portfolio", displayMode: .large)
+            .background(Color("background"))
         }
     }
 }
