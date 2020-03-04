181f31cc5386aa5f9dc9409b59df59b2f0e7712d
Henry Paulino <henrypl360@gmail.com>
2020-03-04 01:10:35 -0500

    show real stock stats

--- StocketiOS/Features/Stock/StockView.swift
+++ StocketiOS/Features/Stock/StockView.swift
@@ -7,12 +7,14 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct StockView: View {
-    var symbol: String
+    var data: [String: String]
     
-    init(symbol: String) {
-        self.symbol = symbol
+    init(data: [String: String]) {
+        self.data = data
+        print(data)
     }
 
     var body: some View {
@@ -21,30 +23,30 @@ struct StockView: View {
                 HStack {
                     Spacer()
                 }
-                Text(self.symbol)
-                    .font(.system(size: 24, weight: .heavy))
-                Text("Company Inc.")
-                    .font(.system(size: 18))
+                LineGraph(name: self.data["name"]!, symbol: self.data["symbol"]!)
             }
             
             VStack(alignment: .leading) {
                 Text("Stats")
+                    .font(.system(size: 20, weight: .heavy))
+                    .padding(.bottom, 20)
                 HStack {
                     VStack {
-                        StatsItem(label: "open")
-                        StatsItem(label: "high")
-                        StatsItem(label: "low")
-                        StatsItem(label: "52 WK high")
-                        StatsItem(label: "52 WK low")
-                        
+                        StatsItem(label: "open", value: self.data["price_open"]!)
+                        StatsItem(label: "high", value: self.data["day_high"]!)
+                        StatsItem(label: "low", value: self.data["day_low"]!)
+                        StatsItem(label: "52 WK high", value: self.data["52_week_high"]!)
+                        StatsItem(label: "52 WK low", value: self.data["52_week_low"]!)
                     }
+                    .padding(.trailing, 5)
                     VStack {
-                        StatsItem(label: "vol")
-                        StatsItem(label: "avg vol")
-                        StatsItem(label: "p/e ratio")
-                        StatsItem(label: "yield")
-                        StatsItem(label: "mkt cap")
+                        StatsItem(label: "vol", value: Int(self.data["volume"]!)!.formatUsingAbbrevation())
+                        StatsItem(label: "avg vol", value: Int(self.data["volume_avg"]!)!.formatUsingAbbrevation())
+                        StatsItem(label: "eps", value: self.data["eps"]!)
+                        StatsItem(label: "shares", value: Int(self.data["shares"]!)!.formatUsingAbbrevation())
+                        StatsItem(label: "mkt cap", value: Int(self.data["market_cap"]!)!.formatUsingAbbrevation())
                     }
+                    .padding(.leading, 5)
                 }
             }
             Spacer()
@@ -55,6 +57,6 @@ struct StockView: View {
 
 struct StockView_Previews: PreviewProvider {
     static var previews: some View {
-        StockView(symbol: "AAPL")
+        StockView(data: ["something": "else"])
     }
 }
